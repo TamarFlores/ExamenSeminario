ac6e2783c57632c4cecf2617a31fc71faaccb0f6
Tamar <tamiflores1@gmail.com>
2021-02-27 03:24:58 -0400

    test 1 clase de apirest

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var indexControler = new IndexController();
 /* GET home page. */
 router.get("/", indexControler.index);
-
+router.post("/plus", indexControler.plus);
+router.post("/ecuation", indexControler.ecuationSolve);
+router.post("/ecuationimg", indexControler.ecuationSolveImg);
 export default router;
